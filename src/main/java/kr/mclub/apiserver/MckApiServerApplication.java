c6a7ca35adf64fa2f4d95a291c7e771b0b812a9a
yeongyu <yeongyu.yang@theragenbio.com>
2025-12-30 10:37:11 +0900

    Initialize Spring Boot project with basic configurations and Gradle setup.

---
+++ src/main/java/kr/mclub/apiserver/MckApiServerApplication.java
@@ -0,0 +1,13 @@
+package kr.mclub.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MckApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MckApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/mclub/apiserver/MckApiServerApplicationTests.java
@@ -0,0 +1,14 @@
+package kr.mclub.apiserver;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MckApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
